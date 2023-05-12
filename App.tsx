7e15110f1732cd01f9c0d063a7acdf7ee681f30b
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-11 21:47:03 -0300

    Setting the application theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 import { StatusBar } from 'expo-status-bar';
 import { Groups } from './src/screens/Groups';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
       <Groups />
-    </>
+    </ThemeProvider>
   );
 }
 
