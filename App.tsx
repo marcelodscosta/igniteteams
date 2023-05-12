5fe6e4d6fb9d15c8fe1451dcc8418c59dc32d3ed
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-12 09:16:22 -0300

    custom fonts

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
 import { ThemeProvider } from 'styled-components';
+import { ActivityIndicator } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import theme from './src/theme';
 import { StatusBar } from 'expo-status-bar';
 import { Groups } from './src/screens/Groups';
 
+
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+
+
   return (
+
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
-
-
